6c6e50dcc7e01b7101872612f82c8b0fce24006f
gmzucolo <gustavo.zucolo@hotmail.com>
2022-09-07 18:37:03 -0300

    splash activity done

---
+++ app/src/main/java/com/example/open_food_facts_sample/ui/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.open_food_facts_sample.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.open_food_facts_sample.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler.postDelayed({ openHomeFragment() }, 2000)
+    }
+
+    private fun openHomeFragment() {
+        val intent = Intent(this@SplashActivity, NavigationActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
